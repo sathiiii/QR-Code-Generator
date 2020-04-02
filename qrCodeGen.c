2a8a10239f5aab41688244353fb8d3c6c7f5ebb4
sathiiii <42441482+sathiiii@users.noreply.github.com>
2020-04-02 08:50:51 +0530

    Update qrCodeGen.c

--- qrCodeGen.c
+++ qrCodeGen.c
@@ -1,35 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-#define BLACK 0
-#define RED 1
-#define GREEN 2
-#define YELLOW 3
-#define BLUE 4
+#define BLACK 	0
+#define RED 	1
+#define GREEN 	2
+#define YELLOW 	3
+#define BLUE 	4
 #define MAGENTA 5
-#define CYAN 6
-#define WHITE 7
+#define CYAN 	6
+#define WHITE 	7
+
+#define BASIC	0
+#define FULL	1
+
+char *colors[7] = {"black", "red", "green", "yellow", "blue", "magenta", "cyan"};
 
 const int SMALL[6][6] = {{-1, -1, 16, 17, -1, -1},
 						 {-1, -1, 18, 19, -1, -1},
-						 {20, 21, 0, 1, 2, 3},
-						 {22, 23, 4, 5, 6, 7},
-						 {-1, -1, 8, 9, 10, 11},
+						 {20, 21,  0,  1,  2,  3},
+						 {22, 23,  4,  5,  6,  7},
+						 {-1, -1,  8,  9, 10, 11},
 						 {-1, -1, 12, 13, 14, 15}};
 
-const int BIG[12][12] = {{-1, -1, 100, 101, 102, 103, 104, 105, 106, 107, -1, -1},
-						 {-1, -1, 108, 109, 110, 111, 112, 113, 114, 115, -1, -1},
-						 {116, 117, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-						 {118, 119, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
-						 {120, 121, 20, 21, 22, 23, 1, 1, 1, 1, 1, 2},
-						 {122, 123, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3},
-						 {124, 125, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5},
-						 {126, 127, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6},
-						 {128, 129, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7},
-						 {130, 131, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8},
-						 {-1, -1, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9},
-						 {-1, -1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 99}};
+const int BIG[12][12] = {{-1,   -1, 100, 101, 102, 103, 104, 105, 106, 107, -1, -1},
+						 {-1,   -1, 108, 109, 110, 111, 112, 113, 114, 115, -1, -1},
+						 {116, 117,   0,   1,   2,   3,   4,   5,   6,   7,  8,  9},
+						 {118, 119,  10,  11,  12,  13,  14,  15,  16,  17, 18, 19},
+						 {120, 121,  20,  21,  22,  23,   1,   1,   1,   1,  1,  2},
+						 {122, 123,   2,   2,   2,   2,   2,   2,   2,   2,  2,  3},
+						 {124, 125,   3,   3,   3,   3,   3,   3,   3,   3,  3,  4},
+						 {126, 127,   4,   4,   4,   4,   4,   4,   4,   4,  4,  5},
+						 {128, 129,   5,   5,   5,   5,   5,   5,   5,   5,  5,  6},
+						 {130, 131,   6,   6,   6,   6,   6,   6,   6,   6,  6,  7},
+						 { -1,  -1,   7,   7,   7,   7,   7,   7,   7,   7,  7,  8},
+						 { -1,  -1,   8,   8,   8,   8,  10,   8,   8,   8,  8, 99}};
+
+int stringCompare(char *str1, char *str2)
+{
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	if (len1 != len2)
+		return 0;
+	for (int i = 0; i < len1; i++)
+		if (str1[i] != str2[i])
+			return 0;
+	return 1;
+}
+
+int indexOf(char *string, char **array)
+{
+	for (int i = 0; i < 7; i++)
+	{
+		if (stringCompare(array[i], string))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int contains(char *string, char **array)
+{
+	if (indexOf(string, array) != -1)
+		return 1;
+	return 0;
+}
 
 void strip(char arr[], int length)
 {
@@ -57,9 +94,19 @@ void clearScreen()
 	printf("\e[1;1H\e[2J");
 }
 
+void changeTextColor(int foregroundColor)
+{
+	printf("%c[%dm", 0x1B, foregroundColor + 30);
+}
+
+void resetColor()
+{
+	printf("%c[0m", 0x1B);
+}
+
 void printBlock(int foregroundColor)
 {
-	printf("%c[%dm%c%c%c[0m", 0x1B, foregroundColor + 30, 219, 219, 0x1B);
+	printf("%c[%d;%dm%*c%c[0m", 0x1B, 30, 40 + foregroundColor, 2, ' ', 0x1B);
 }
 
 char *hash(char str[])
@@ -77,55 +124,190 @@ char *hash(char str[])
 	return hashed;
 }
 
-void applyPadding(unsigned long long grid[38], int gridSize)
+void applyPadding(int grid[38][38], int gridSize)
 {
-	grid[0] = 1;
-	for (int i = 1; i < gridSize; i++)
+	for (int i = 0; i < gridSize; i++)
 	{
-		grid[0] = (grid[0] << 1) | 1;
-	}
-	grid[gridSize - 1] = grid[0];
-	for (int i = 1; i < gridSize - 1; i++)
-	{
-		grid[i] = (1 << (gridSize - 1)) | 1;
+		for (int j = 0; j < gridSize; j++)
+		{
+			grid[i][j] = 0;
+		}
 	}
 }
 
-void maskPositionIndicators(unsigned long long grid[38], int gridSize)
+void maskPositionIndicators(int grid[38][38], int gridSize)
 {
-	grid[2] |= 15 << (gridSize - 6);
-	grid[3] |= 9 << (gridSize - 6);
-	grid[4] = grid[3];
-	grid[5] |= grid[2];
-	grid[gridSize - 3] = grid[2];
-	grid[gridSize - 4] = grid[3];
-	grid[gridSize - 5] = grid[3];
-	grid[gridSize - 6] = grid[2];
-	grid[2] |= 15 << 2;
-	grid[3] |= 9 << 2;
-	grid[4] = grid[3];
-	grid[5] = grid[2];
+	for (int j = 1; j < 7; j++)
+	{
+ 		grid[1][j] = 1;
+		grid[6][j] = 1;
+		grid[1][gridSize - j - 1] = 1;
+		grid[6][gridSize - j - 1] = 1;
+		grid[gridSize - 7][j] = 1;
+		grid[gridSize - 2][j] = 1;
+	}
+	for (int i = 2; i < 6; i++)
+	{
+		grid[i][1] = 1;
+		grid[i][6] = 1;
+		grid[i][gridSize - 7] = 1;
+		grid[i][gridSize - 2] = 1;
+		grid[gridSize - i - 1][1] = 1;
+		grid[gridSize - i - 1][6] = 1;
+	}
+	grid[3][3] = 1;
+	grid[3][4] = 1;
+	grid[3][gridSize - 5] = 1;
+	grid[3][gridSize - 4] = 1;
+	grid[4][3] = 1;
+	grid[4][4] = 1;
+	grid[4][gridSize - 5] = 1;
+	grid[4][gridSize - 4] = 1;
+	grid[gridSize - 5][3] = 1;
+	grid[gridSize - 5][4] = 1;
+	grid[gridSize - 4][3] = 1;
+	grid[gridSize - 4][4] = 1;
+	
+}
+
+void fillHashData(char *hashedStr, int grid[38][38], int gridSize)
+{
+	if (gridSize == 20)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				if (SMALL[i][j] != -1)
+				{
+					int val = (int)hashedStr[SMALL[i][j]];
+					for (int k = 0; k < 3; k ++)
+					{
+						for (int l = 0; l < 3; l++)
+						{
+							grid[1 + 3 * i + k][1 + 3 * j + l] = (val >> (8 - (3 * k + l))) & 1;
+						}
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			for (int j = 0; j < 12; j++)
+			{
+				if (BIG[i][j] != -1)
+				{
+					int val = (int)hashedStr[BIG[i][j]];
+					for (int k = 0; k < 3; k ++)
+					{
+						for (int l = 0; l < 3; l++)
+						{
+							grid[1 + 3 * i + k][1 + 3 * j + l] = (val >> (8 - (3 * k + l))) & 1;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void printHelp(char *programName, int type)
+{
+	printf("Blah blah blah\n");
 }
 
 int main(int argc, char *argv[])
 {
-	/* char url[120];
+	int foregroundColor = BLACK;
+	if (argc == 2)
+	{
+		if (stringCompare(argv[1], "-h"))
+		{
+			printf("\n");
+			printHelp(argv[0], FULL);
+			return 0;
+		}
+		else if (stringCompare(argv[1], "-c"))
+		{
+			changeTextColor(RED);
+			printf("Missing 1 argument:\n");
+			resetColor();
+			printf("\tColor must be specified just after the -c flag.\n\n");
+			printHelp(argv[0], BASIC);
+			return 0;
+		}
+		else
+		{
+			changeTextColor(RED);
+			printf("Incorrect usage of arguments:\n");
+			resetColor();
+			printf("\tView help for more details.\n\n");
+			printHelp(argv[0], BASIC);
+			return 0;
+		}
+	}
+	else if (argc == 3)
+	{
+		if (stringCompare(argv[1], "-c") && contains(argv[2], colors))
+		{
+			foregroundColor = indexOf(argv[2], colors);
+		}
+		else if (stringCompare(argv[1], "-c") && !contains(argv[2], colors))
+		{
+			changeTextColor(RED);
+			printf("Color undefined:\n");
+			resetColor();
+			printf("\tView help for more details.\n\n");
+			printHelp(argv[0], BASIC);
+			return 0;
+		}
+		else
+		{
+			changeTextColor(RED);
+			printf("Incorrect usage of arguments:\n");
+			resetColor();
+			printf("\tView help for more details.\n\n");
+			printHelp(argv[0], BASIC);
+			return 0;
+		}
+	}
+	else if (argc > 3)
+	{
+		changeTextColor(RED);
+		printf("The arguments list is too long.\n\n");
+		resetColor();
+		printHelp(argv[0], BASIC);
+		return 0;
+	}
+	char url[300];
 	clearScreen();
 	printf("Enter the URL: ");
 	scanf("%s", url);
 	clearScreen();
 	char *hashedStr = hash(url);
-	int qrGridSize = (strlen(hashedStr) == 24) ? 18 + 2 : 36 + 2;
-	unsigned long long grid[38];
+	int qrGridSize = (strlen(hashedStr) == 24) ? 20 : 38;
+	int grid[38][38] = {{-1}};
 	applyPadding(grid, qrGridSize);
-	maskPositionIndicators(grid, qrGridSize); */
-
-	for (int i = 0; i < 100; i++)
+	maskPositionIndicators(grid, qrGridSize);
+	fillHashData(hashedStr, grid, qrGridSize);
+	for (int i = 0; i < qrGridSize; i++)
 	{
-		printf("%c[%d;%dm", 0x1B, 30, 47);
-		printf(" ");
-		printf("%c[0m", 0x1B);
+		for (int j = 0; j < qrGridSize; j++)
+		{
+			if (grid[i][j] == 0)
+			{
+				printBlock(WHITE);
+			}
+			else if (grid[i][j] == 1)
+			{
+				printBlock(foregroundColor);
+			}
+		}
+		printf("\n");
 	}
-
+	printf("\n");
 	return 0;
 }
